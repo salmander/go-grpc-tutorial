77c2d635eb731c0e5410133747e747f1cbd380dc
Salman Ahmed <salman.great@gmail.com>
2020-01-07 13:38:20 +0000

    tidy up hello world example

--- helloworld_rpc_server/main.go
+++ helloworld_rpc_server/main.go
@@ -21,12 +21,13 @@ type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
+// SayHello implements helloworld.GreeterServer interface
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName() + "\n"}, nil
 }
 
+// SayHello implements helloworld.GreeterServer interface
 func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received hello again: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello again " + in.GetName() + "\n"}, nil
